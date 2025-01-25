8a467ce35600f290163bab007c80e986fa8f2e9f
Kimberly <kimhnguyeno@g.ucla.edu>
2025-01-24 16:34:23 -0800

    Initialization of react-native project with Expo.

---
+++ blockhouse-trial/app/_components/UserSignUpForm.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Button, TextInput, TouchableOpacity } from 'react-native';
+
+export default function UserSignUpForm() {
+    return (
+        <View>
+            <Text>SignUp Form</Text>
+        </View>
+    )
+};
